affa328846de2b8e86b5015b058cb139d5a27889
mac <macdonald.robinson@zgm.ca>
2018-12-17 09:43:15 -0700

    Initial Commit

---
+++ jquery-ajax-loader.js
@@ -0,0 +1,74 @@
+(function ($) {
+
+	$.fn.loadeData = function (html) {
+
+		this.each(function (index, el) {
+			$(el).fadeOut("slow", function () {
+				$(el).html(html);
+				$(el).fadeIn("slow");
+			});
+		});
+		
+	}
+
+	$.fn.ajaxLoadUrl = function (href, callBackFunction) {
+
+		this.each(function (index, el) {
+
+			$.get(href, function (data) {
+				var bodyHtml = data;
+
+				var doc = $('<output>').append($.parseHTML(data, document, true));
+
+				document.title = doc.find("title").text();
+				window.history.pushState({ "html": data, "pageTitle": document.title }, "", href);
+
+				bodyHtml = doc.html();
+
+				if (callBackFunction != undefined && callBackFunction != "" && callBackFunction != null) {
+					callBackFunction($(el), bodyHtml);
+				}
+				else {
+					$(el).loadeData(bodyHtml);
+				}
+			});
+
+		});
+
+	}
+
+	$.fn.ajaxAnchor = function (callBackFunction) {
+
+		this.each(function (index, el) {
+			var href = $(el).attr("href");
+
+			$(el).attr("href", "javascript:void(0)");
+			$(el).attr("data-href", href);
+		});
+
+		this.on("click", function () {
+			var href = $(this).attr("data-href");
+			var elementId = $(this).attr("data-ajax-target");
+			var targetElement = $(elementId);
+			
+			targetElement.ajaxLoadUrl(href, callBackFunction);
+
+			//$.get(href, function (data) {
+			//	var bodyHtml = data;
+
+			//	if (loadElement != undefined && loadElement != "" && loadElement != null) {
+			//		var doc = $('<output>').append($.parseHTML(data, document, true));
+
+			//		document.title = doc.find("title").text();
+			//		window.history.pushState({ "html": data, "pageTitle": document.title }, "", href);
+
+			//		bodyHtml = doc.find(loadElement).html();
+			//	}
+
+			//	callBackFunction(targetElement, bodyHtml);
+			//});
+		})
+	};
+
+
+})(jQuery);
